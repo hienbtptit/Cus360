165e45d826b52cbc55b7a4eb5bf36402ffe3fff6
hien.buithi <hienbtptit@gmail.com>
2021-06-16 14:34:09 +0700

    Crawl data from bds123.com: sort date by desc and crawl data as schedule

--- Bs4/BDS123.py
+++ Bs4/BDS123.py
@@ -6,32 +6,30 @@ import os
 import multiprocessing
 import pandas
 import re
-import time
-import test_leveldb
+import datetime
 
+baseUrl = "https://bds123.vn"
+url = "https://bds123.vn/nha-dat-ban-ho-chi-minh.html"
 
-from datetime import datetime
 from bs4 import BeautifulSoup
-def getFinalPage(url):
-    r = requests.get(url)
+def getFinalPage(urlPara):
+    r = requests.get(urlPara)
     soup = BeautifulSoup(r.content, 'html5lib')
     final_page = re.split("=", soup.find_all('a', class_='page-link')[-1]['href'])[-1]
     return int(final_page)
 
 def crawlDataFirstTime(start, end):
-    baseUrl = "https://bds123.vn"
-    url = "https://bds123.vn/nha-dat-ban-ho-chi-minh.html"
     if(end >= getFinalPage(url)): end = getFinalPage(url) + 1
     print("run from " + str(start) + " to " + str(end))
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     print("Start"+current_time);
-    file_path = os.getcwd()+"\\"+"bds1231.csv"
+    file_path = os.getcwd()+"\\"+"bds123.csv"
 
     for i in range(start, end):
         l = []
         try:
-            r = requests.get(url+'?page='+str(i))
+            r = requests.get(url+'?created=desc&page='+str(i))
             soup = BeautifulSoup(r.content, 'html5lib')
         except:
             print("Page Url Error: ")
@@ -42,15 +40,14 @@ def crawlDataFirstTime(start, end):
                 d = {}
                 try:
                     href = link['href']
-                  #  print(baseUrl + href)
                     request = requests.get(baseUrl + href)
                 except:
                     print("Post Url Error: ")
                     print(baseUrl + href)
                     continue
-                if test_leveldb.check_exist(baseUrl+href) == 1: continue
+                ''' if test_leveldb.check_exist(baseUrl+href) == 1: continue
                 else:
-                    test_leveldb.insert_link(baseUrl+href)
+                    test_leveldb.insert_link(baseUrl+href)'''
                 try:
                     soup = BeautifulSoup(request.content, 'html5lib')
                     d['prid'] = soup.find('section', class_='section-post-detail')['data-id']
@@ -66,19 +63,76 @@ def crawlDataFirstTime(start, end):
         df= pandas.DataFrame(l)
         df.to_csv(file_path, mode="a", header=False, index=False, na_rep="NaN",quoting=csv.QUOTE_ALL)
 
+def crawlBySchedule(year, month, day): #crawl data after day : day-month-year
+    stop = 0 # stop while loop when stop = 1
+    page = 1
+    file_path = os.getcwd() + "\\" + "bds123-"+str(year)+str(month)+str(day)+".csv"
+    #now = re.split("\s",str(datetime.datetime.now()))[0]
+    #now = re.split("-",date)
+    #now = datetime.datetime(int(now[0]), int(now[1]), int(now[2]))
+    now = datetime.datetime(year,month,day)
+    while stop == 0:
+        l = []
+        try:
+            r = requests.get(url + '?created=desc&page=' + str(page))
+            soup = BeautifulSoup(r.content, 'html5lib')
+        except:
+            print("Page Url Error: ")
+            print(url + '?page=' + str(page))
+            continue
+        list_link = soup.find_all('a', class_='link')
+        for link in list_link:
+            d = {}
+            try:
+                href = link['href']
+                request = requests.get(baseUrl + href)
+            except:
+                print("Post Url Error: ")
+                print(baseUrl + href)
+                continue
+            ''' if test_leveldb.check_exist(baseUrl+href) == 1: continue
+            else:
+                test_leveldb.insert_link(baseUrl+href)'''
+            try:
+                soup = BeautifulSoup(request.content, 'html5lib')
+                d['prid'] = soup.find('section', class_='section-post-detail')['data-id']
+                d['title'] = soup.find('h1', class_='post-h1').text
+                d['des'] = soup.find('div', class_='post-description').text
+                d['phone'] = soup.find('button', class_='author-phone').text
+                time = soup.find('div', class_='item published')
+                d['time'] = time.findChild('span')['title']
+                timePost = re.split("\s",d['time'])[-1]
+                timePost = re.split("/", timePost)
+                timePost = datetime.datetime(int(timePost[2]), int(timePost[1]), int(timePost[0]))
+                #check if timepost < date then break the loop
+                if (timePost < now) :
+                    stop = 1
+                    break
+            except:
+                print("Get atribute value error")
+                continue
+            l.append(d)
+        df = pandas.DataFrame(l)
+        df.to_csv(file_path, mode="a", header=False, index=False, na_rep="NaN", quoting=csv.QUOTE_ALL)
+        page = page + 1
+
+
 if __name__ == '__main__':
-    final = getFinalPage('https://bds123.vn/nha-dat-ban-ho-chi-minh.html')
-    numProcess = 13 # run 13 process
+    crawlBySchedule(2021,6,16)
+    '''final = getFinalPage('https://bds123.vn/nha-dat-ban-ho-chi-minh.html')
+    numProcess = multiprocessing.cpu_count()*2 - 4 # run 13 process
+
+    print(numProcess)
     #print("Final page: "+str(final)+" / " + str(final/numProcess))
     ### Multiprocessing with Process
-    #processes=[Process(target=crawlDataFirstTime,args=(i,i+int(final/numProcess))) for i in range(1, final, int(final/numProcess))] #init numProcess process
+    processes=[Process(target=crawlDataFirstTime,args=(i,i+int(final/numProcess))) for i in range(1, final, int(final/numProcess))] #init numProcess process
 
     #multithreading
-    threads=[Thread(target=crawlDataFirstTime,args=(i,i+int(final/numProcess))) for i in range(1, final, int(final/numProcess))]
+    #threads=[Thread(target=crawlDataFirstTime,args=(i,i+int(final/numProcess))) for i in range(1, final, int(final/numProcess))]
+
     # Run processes
-    #for p in processes:p.start()
-    for thread in threads: thread.start()
+    for p in processes:p.start()
     # Exit the completed processes
-    #for p in processes:p.join()
-    for thread in threads: thread.join()
+    for p in processes:p.join()'''
+
 

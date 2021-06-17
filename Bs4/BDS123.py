6c14eba47dad0454feae2a1da3ddce6181444201
hien.buithi <hienbtptit@gmail.com>
2021-06-17 16:31:16 +0700

    Check duplicate link using level db

--- Bs4/BDS123.py
+++ Bs4/BDS123.py
@@ -1,12 +1,14 @@
 from multiprocessing import Process
-from threading import Thread
+import multiprocessing
 import requests
 import csv
 import os
-import multiprocessing
 import pandas
 import re
 import datetime
+import sys
+sys.path.append('../')
+from CustomLibs import single_thread_leveldb
 
 baseUrl = "https://bds123.vn"
 url = "https://bds123.vn/nha-dat-ban-ho-chi-minh.html"
@@ -35,16 +37,13 @@ def crawlDataFirstTime(start, end):
         list_link = soup.find_all('a', class_='link')
         for  link in  list_link:
                 d = {}
+                href = link['href']
                 try:
-                    href = link['href']
                     request = requests.get(baseUrl + href)
                 except:
                     print("Post Url Error: ")
                     print(baseUrl + href)
                     continue
-                ''' if test_leveldb.check_exist(baseUrl+href) == 1: continue
-                else:
-                    test_leveldb.insert_link(baseUrl+href)'''
                 try:
                     soup = BeautifulSoup(request.content, 'html5lib')
                     d['prid'] = soup.find('section', class_='section-post-detail')['data-id']
@@ -66,14 +65,15 @@ def crawlDataFirstTime(start, end):
         df= pandas.DataFrame(l)
         df.to_csv(file_path, mode="a", header=False, index=False, na_rep="NaN",quoting=csv.QUOTE_ALL)
 
-def crawlBySchedule(year, month, day): #crawl data after day : day-month-year
-    stop = 0 # stop while loop when stop = 1
+def crawlBySchedule(): #crawl data after day : day-month-year
+    stop = 0 # stop while loop when 4 url is duplicate
     page = 1
-    file_path = os.getcwd() + "\\" + "bds123-"+str(year)+str(month)+str(day)+".csv"
-    #now = re.split("\s",str(datetime.datetime.now()))[0]
-    #now = re.split("-",date)
-    #now = datetime.datetime(int(now[0]), int(now[1]), int(now[2]))
-    now = datetime.datetime(year,month,day)
+    now = re.split("\s",str(datetime.datetime.now()))[0]
+    now = re.split("-",now)
+
+    file_path = os.getcwd() + "\\" + "bds123-" + now[0] + now[1] + now[1] + ".csv"
+    print(file_path)
+    iterator = 0
     while stop == 0:
         l = []
         try:
@@ -85,17 +85,23 @@ def crawlBySchedule(year, month, day): #crawl data after day : day-month-year
             continue
         list_link = soup.find_all('a', class_='link')
         for link in list_link:
+            href = link['href']
             d = {}
+            if single_thread_leveldb.check_exist(baseUrl + href) == 1:
+                if iterator == 3 :
+                    stop = 1
+                    break
+                iterator = iterator+1
+            else:
+                iterator = 0
+                single_thread_leveldb.insert_link(baseUrl + href)
+
             try:
-                href = link['href']
                 request = requests.get(baseUrl + href)
             except:
                 print("Post Url Error: ")
                 print(baseUrl + href)
                 continue
-            ''' if test_leveldb.check_exist(baseUrl+href) == 1: continue
-            else:
-                test_leveldb.insert_link(baseUrl+href)'''
             try:
                 soup = BeautifulSoup(request.content, 'html5lib')
                 d['prid'] = soup.find('section', class_='section-post-detail')['data-id']
@@ -113,16 +119,16 @@ def crawlBySchedule(year, month, day): #crawl data after day : day-month-year
             timePost = re.split(":", timePost)
             d['time'] = datetime.datetime(int(dayPost[2]), int(dayPost[1]), int(dayPost[0]), int(timePost[0]), int(timePost[1]))
             #check if timepost < date then break the loop
-            if (d['time']  < now) :
-                stop = 1
-                break
+
             l.append(d)
         df = pandas.DataFrame(l)
         df.to_csv(file_path, mode="a", header=False, index=False, na_rep="NaN", quoting=csv.QUOTE_ALL)
         page = page + 1
+        if stop == 1:
+            break
 
 
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     #crawlBySchedule(2021,6,16)
     final = getFinalPage('https://bds123.vn/nha-dat-ban-ho-chi-minh.html')
     numProcess = multiprocessing.cpu_count()*2 - 4 # run process
@@ -138,6 +144,40 @@ if __name__ == '__main__':
     # Run processes
     for p in processes:p.start()
     # Exit the completed processes
-    for p in processes:p.join()
+    for p in processes:p.join()'''
+
+
+
+keys = sys.argv[1::2]
+values = sys.argv[2::2]
+
+print(keys)
+print(values)
+
+args = {k: v for k, v in zip(keys, values)}
+print(args)
+
+first_time = args.get('--first-time')
+if first_time == 1 :
+    print("crawlDataFirstTime")
+    final = getFinalPage('https://bds123.vn/nha-dat-ban-ho-chi-minh.html')
+    numProcess = multiprocessing.cpu_count() * 2 - 4  # run process
+
+    print(numProcess)
+    # print("Final page: "+str(final)+" / " + str(final/numProcess))
+    ### Multiprocessing with Process
+    processes = [Process(target=crawlDataFirstTime, args=(i, i + int(final / numProcess))) for i in
+                 range(1, final, int(final / numProcess))]  # init numProcess process
+
+    # multithreading
+    # threads=[Thread(target=crawlDataFirstTime,args=(i,i+int(final/numProcess))) for i in range(1, final, int(final/numProcess))]
+
+    # Run processes
+    for p in processes: p.start()
+    # Exit the completed processes
+    for p in processes: p.join()
+else:
+    print("crawlBySchedule")
+    crawlBySchedule()
 
 
